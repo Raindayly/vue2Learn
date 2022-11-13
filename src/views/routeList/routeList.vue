11d3fba5e39a9c2f354401b06f5b22194c0b0211
yly <fppuz123456@163.com>
2022-11-13 15:49:10 +0800

    添加路由访问页面，远程搜索配合默认数据研究

---
+++ src/views/routeList/routeList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-for="(item,index) in routeList" :key="index">
+            <p> 名字:{{ item.name }} </p>
+            <p> 路径:{{ 'http://localhost:8080/'  + item.path }} </p>
+            <p> 组件:{{ item.component }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import {routes} from '@/router/router'
+export default {
+    data(){
+        return {
+            routeList:routes[0].children,
+            a:1
+        }
+    },
+    mounted(){
+        console.log(this.routeList);
+    }
+}
+</script>
+
+<style>
+
+</style>
